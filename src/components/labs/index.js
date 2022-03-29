2d13d1f5049be31eaf3a3e641302e97cc93de8a3
Shawn Robin <robin.sh@northeastern.edu>
2022-03-28 23:21:05 -0400

    remove a6 lab components

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import ConditionalOutput from "./conditional-output";
-import Styles from "./styles";
-import Classes from "./classes";
-import TodoList from "./todo/todo-list";
 import ReduxExamples from "./redux-examples";
 
 const Labs = () => {
   return (
     <>
       <h1>Labs</h1>
-      <TodoList />
-      <ConditionalOutput />
-      <Styles />
-      <Classes />
-      <Link to="/hello">Hello</Link> | <Link to="/tuiter/">Tuiter</Link>
+      <Link to="/tuiter/">Tuiter</Link>
       <ReduxExamples />
     </>
   );
