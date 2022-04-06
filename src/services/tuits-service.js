c8532477199c298a9d92de2a650c8698a4a1721c
Shawn Robin <robin.sh@northeastern.edu>
2022-04-06 17:58:41 -0400

    use environment variable to set api url

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const TUITS_API = "https://cs4550-web-dev-node-server.herokuapp.com/api/tuits";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const findAllTuits = async () => {
   const response = await axios.get(TUITS_API);
