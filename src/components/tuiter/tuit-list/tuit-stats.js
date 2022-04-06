58153f1d6e908397f73b85869aee4a11cf9ce858
Shawn Robin <robin.sh@northeastern.edu>
2022-04-06 17:58:25 -0400

    formatting

--- src/components/tuiter/tuit-list/tuit-stats.js
+++ src/components/tuiter/tuit-list/tuit-stats.js
@@ -6,28 +6,32 @@ const TuitStats = ({ tuit }) => {
 
   return (
     <>
-      <span
-        className="wd-symbol wd-interact"
-
-      >
-        {<i className="far fa-thumbs-up" onClick={() =>
-          updateTuit(dispatch, {
-            ...tuit,
-            likes: tuit.likes + 1,
-          })
-        }></i>}
+      <span className="wd-symbol wd-interact">
+        {
+          <i
+            className="far fa-thumbs-up"
+            onClick={() =>
+              updateTuit(dispatch, {
+                ...tuit,
+                likes: tuit.likes + 1,
+              })
+            }
+          ></i>
+        }
         {<span className="wd-symbol-count">{tuit.likes}</span>}
       </span>
-      <span
-        className="wd-symbol wd-interact"
-
-      >
-        {<i className="far fa-thumbs-down" onClick={() =>
-          updateTuit(dispatch, {
-            ...tuit,
-            dislikes: tuit.dislikes + 1,
-          })
-        }></i>}
+      <span className="wd-symbol wd-interact">
+        {
+          <i
+            className="far fa-thumbs-down"
+            onClick={() =>
+              updateTuit(dispatch, {
+                ...tuit,
+                dislikes: tuit.dislikes + 1,
+              })
+            }
+          ></i>
+        }
         {<span className="wd-symbol-count">{tuit.dislikes}</span>}
       </span>
     </>
